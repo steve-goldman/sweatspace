582cdce7f50eb6cf2412f25c16ac3d6d04ead3c3
Steve Goldman <steve.goldman@gmail.com>
2016-01-12 23:50:06 -0500

    validations

--- app/models/clazz_type.rb
+++ app/models/clazz_type.rb
@@ -1,3 +1,9 @@
 class ClazzType < ActiveRecord::Base
+  PERMITTED_PARAMS = [
+    "name"
+  ]
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
   has_paper_trail
 end
--- spec/models/clazz_type_spec.rb
+++ spec/models/clazz_type_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe ClazzType, :type => :model do
   it "has a valid factory" do
     expect(FactoryGirl.build(:clazz_type)).to be_valid
   end
+
+  describe "validations" do
+    subject { FactoryGirl.build :clazz_type }
+    it { should validate_presence_of :name }
+    it { should validate_uniqueness_of :name }
+  end
 end
