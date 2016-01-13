c6b8075ec7bf220fc481aa388e24ae94e27afeca
Steve Goldman <steve.goldman@gmail.com>
2016-01-13 14:51:47 -0500

    now show for class types and studios, and keep the wildcard at the bottom

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#show"
-  resources :instructor_profiles, except: :destroy
-  get "/:id", to: "instructor_profiles#show", as: :profile
 
   namespace :admin do
-    resources :clazz_types, except: :destroy
-    resources :studios, except: :destroy
+    resources :clazz_types, except: [:show, :destroy]
+    resources :studios, except: [:show, :destroy]
   end
+
+  resources :instructor_profiles, except: :destroy
+  get "/:id", to: "instructor_profiles#show", as: :profile
 end
