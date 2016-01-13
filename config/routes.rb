8109df01a8f324447e3dbc13226ba6bc72ff9bff
Steve Goldman <steve.goldman@gmail.com>
2016-01-12 21:13:34 -0500

    profile path link

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#show"
   resources :instructor_profiles
-  get "/:id", to: "instructor_profiles#show"
+  get "/:id", to: "instructor_profiles#show", as: :profile
 end
