5a408f7d539ff06b06fa3401685e9b493b98dfeb
Steve Goldman <steve.goldman@gmail.com>
2016-01-15 00:01:10 -0500

    tell devise that email isn't required

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :trackable, :validatable, :omniauthable, :registerable
-  validates :email, email: true, unless: :uid?
+  validates :email, email: true, if: :email?
   has_one :instructor_profile
   acts_as_paranoid
+
+  def email_required?
+    false
+  end
 
   def self.from_omniauth auth
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
