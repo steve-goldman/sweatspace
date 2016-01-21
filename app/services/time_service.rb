73c9f16bbb270c6bb76e4d0db044b9317cf7e637
Steve Goldman <steve.goldman@gmail.com>
2016-01-20 23:38:29 -0500

    time fix

--- app/services/time_service.rb
+++ app/services/time_service.rb
@@ -7,7 +7,7 @@ class TimeService
       time_tokens = /(\d\d):(\d\d) (AM|PM)/.match(time)
       if time_tokens
         Time.new date_tokens[1], date_tokens[2], date_tokens[3],
-                 time_tokens[3] == "AM" ? time_tokens[1] : time_tokens[1].to_i + 12, time_tokens[2], 0,
+                 time_tokens[3] == "AM" ? (time_tokens[1].to_i % 12) : (time_tokens[1].to_i % 12 + 12), time_tokens[2], 0,
                  ActiveSupport::TimeZone.new(timezone).formatted_offset
       end
     end
