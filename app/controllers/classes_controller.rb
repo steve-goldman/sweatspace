0a2db199d85526fd1f5ea99ea67ff5f4cb355c6c
Steve Goldman <steve.goldman@gmail.com>
2016-01-25 22:29:56 -0500

    cleaner controller for classes index, changed layout to aggregate by date

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -2,18 +2,27 @@ class ClassesController < ApplicationController
   DEFAULT_RANGE_SPAN = 7
 
   def index
-    @clazzes = current_user.instructor_profile.classes.confirmed
-      .order(timestamp: :asc)
-      .where(timestamp: date_range.range).decorate
-    @min_date = date_range.min_date
-    @max_date = date_range.max_date
-    @prev_min_date = date_range.prev_min_date
-    @prev_max_date = date_range.prev_max_date
-    @next_min_date = date_range.next_min_date
-    @next_max_date = date_range.next_max_date
+    @classes_by_date = classes_by_date
+    @date_range = date_range
+    @presenter = InstructorClassesPresenter.new classes_by_date, date_range
   end
 
   private
+
+  def classes_by_date
+    @classes_by_date ||=
+      begin
+        classes.each_with_object({}) do |clazz, classes_by_date|
+          (classes_by_date[clazz.date] ||= []).push clazz
+        end
+      end
+  end
+
+  def classes
+    @classes ||= current_user.instructor_profile.classes.confirmed
+      .order(timestamp: :asc)
+      .where(timestamp: date_range.range).decorate
+  end
 
   def date_range
     @date_range ||= DateRange.new DEFAULT_RANGE_SPAN, current_user, params[:min_date], params[:max_date]
