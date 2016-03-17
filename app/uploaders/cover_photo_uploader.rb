11d815b61f47234dd8728952338342d3571698dd
Steve Goldman <steve.goldman@gmail.com>
2016-03-17 18:02:00 -0400

    unique filenames

--- app/uploaders/cover_photo_uploader.rb
+++ app/uploaders/cover_photo_uploader.rb
@@ -48,8 +48,14 @@ class CoverPhotoUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename
+  end
 
+  private
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) || model.instance_variable_set(var, UUIDTools::UUID.random_create.to_s)
+  end
 end
