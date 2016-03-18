9b906bb7494548648114a08ce48dfe80aecf7df3
Steve Goldman <steve.goldman@gmail.com>
2016-03-18 11:08:22 -0400

    carrierwave file storage for development

--- app/uploaders/cover_photo_uploader.rb
+++ app/uploaders/cover_photo_uploader.rb
@@ -4,8 +4,6 @@ class CoverPhotoUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
 
-  storage :aws
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,26 @@
 require 'carrierwave/orm/activerecord'
 
 CarrierWave.configure do |config|
-  config.storage = :aws
-  config.aws_bucket = Figaro.env.aws_s3_bucket_name
-  config.aws_acl = "public-read"
+  if Rails.env.production?
+    config.storage = :aws
+    config.aws_bucket = Figaro.env.aws_s3_bucket_name
+    config.aws_acl = "public-read"
 
-  # The maximum period for authenticated_urls is only 7 days.
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+    # The maximum period for authenticated_urls is only 7 days.
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
-  # Set custom options such as cache control to leverage browser caching
-  config.aws_attributes = {
-    expires: 1.week.from_now.httpdate,
-    cache_control: 'max-age=604800'
-  }
+    # Set custom options such as cache control to leverage browser caching
+    config.aws_attributes = {
+      expires: 1.week.from_now.httpdate,
+      cache_control: 'max-age=604800'
+    }
 
-  config.aws_credentials = {
-    access_key_id: Figaro.env.aws_access_key,
-    secret_access_key: Figaro.env.aws_secret_access_key,
-    region: Figaro.env.aws_s3_region,
-  }
+    config.aws_credentials = {
+      access_key_id: Figaro.env.aws_access_key,
+      secret_access_key: Figaro.env.aws_secret_access_key,
+      region: Figaro.env.aws_s3_region,
+    }
+  else
+    config.storage = :file
+  end
 end
