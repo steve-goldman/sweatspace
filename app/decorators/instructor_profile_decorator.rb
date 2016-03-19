fcecc45468638fd073f15f4bafafb681d37e0a47
Steve Goldman <steve.goldman@gmail.com>
2016-03-19 12:41:53 -0400

    display for social

--- app/decorators/instructor_profile_decorator.rb
+++ app/decorators/instructor_profile_decorator.rb
@@ -5,4 +5,32 @@ class InstructorProfileDecorator < Draper::Decorator
   def full_name
     user.name
   end
+
+  def any_social?
+    [facebook, instagram, twitter, youtube].any? &:present?
+  end
+
+  def facebook_link
+    unless facebook.blank?
+      "https://www.facebook.com/#{facebook}"
+    end
+  end
+
+  def instagram_link
+    unless instagram.blank?
+      "https://www.instagram.com/#{instagram}"
+    end
+  end
+
+  def twitter_link
+    unless twitter.blank?
+      "https://twitter.com/#{twitter}"
+    end
+  end
+
+  def youtube_link
+    unless youtube.blank?
+      "https://www.youtube.com/user/#{youtube}"
+    end
+  end
 end
