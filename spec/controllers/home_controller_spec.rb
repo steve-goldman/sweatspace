cb4083eae19fa553344a25ea6f7de2991db14283
Steve Goldman <steve.goldman@gmail.com>
2016-03-21 14:36:31 -0400

    removing test corresponding to dashboard, which was removed earlier

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,13 +3,6 @@ require "rails_helper"
 RSpec.describe HomeController do
   render_views
 
-  describe "GET :dashboard" do
-    it "renders the view" do
-      get :dashboard
-      expect(response).to render_template(:dashboard)
-    end
-  end
-
   describe "GET :faq" do
     it "renders the view" do
       get :faq
